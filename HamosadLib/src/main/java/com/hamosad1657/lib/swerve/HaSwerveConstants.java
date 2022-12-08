1e13d350a480265d73b8cb763badef61b589ce23
Sufi Levy <96763878+ShakedMev@users.noreply.github.com>
2022-12-08 20:45:45 +0200

    Continued HaSwerveModule (not done but I want the code on GitHub)

---
+++ HamosadLib/src/main/java/com/hamosad1657/lib/swerve/HaSwerveConstants.java
@@ -0,0 +1,6 @@
+package com.hamosad1657.lib.swerve;
+
+public class HaSwerveConstants {
+	public static final double kTalonFXIntegratedSensorTicksPerRev = 2048;
+	public static final double kTalonFXIntegratedSensorTicksPerDegree = 2048/360.0;
+}
--- HamosadLib/src/main/java/com/hamosad1657/lib/swerve/HaSwerveSubsystem.java
+++ HamosadLib/src/main/java/com/hamosad1657/lib/swerve/HaSwerveSubsystem.java
@@ -2,6 +2,6 @@ package com.hamosad1657.lib.swerve;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class HaSwerveSubsystem extends SubsystemBase{
-	
+public class HaSwerveSubsystem extends SubsystemBase {
+
 }
