1b3c9e1760cdda5228b5ce6850de938a60b584cc
Shaked <96763878+ShakedMev@users.noreply.github.com>
2022-12-09 16:18:09 +0200

    Finished all the methods except for optimize()

--- HamosadLib/src/main/java/com/hamosad1657/lib/swerve/HaSwerveConstants.java
+++ HamosadLib/src/main/java/com/hamosad1657/lib/swerve/HaSwerveConstants.java
@@ -3,4 +3,7 @@ package com.hamosad1657.lib.swerve;
 public class HaSwerveConstants {
 	public static final double kTalonFXIntegratedEncoderTicksPerRev = 2048;
 	public static final double kTalonFXIntegratedEncoderTicksPerDegree = 2048/360.0;
+	public static final double kWheelDiameterCM = 10.16;
+	public static final double kWheelCircumferenceCM = kWheelDiameterCM * Math.PI;
+	public static final double kWheelCircumferenceMeters = kWheelCircumferenceCM / 10;
 }
