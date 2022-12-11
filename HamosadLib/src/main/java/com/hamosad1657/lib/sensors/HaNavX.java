f6e65cac2c2dfb149317d1d56b79dd1c88d90e08
Sufi Levy <96763878+ShakedMev@users.noreply.github.com>
2022-12-11 20:44:36 +0200

    Format, make SwerveSubsystem not singleton, remove Spark Max support from class SwerveModule

--- HamosadLib/src/main/java/com/hamosad1657/lib/sensors/HaNavX.java
+++ HamosadLib/src/main/java/com/hamosad1657/lib/sensors/HaNavX.java
@@ -1,7 +1,6 @@
 package com.hamosad1657.lib.sensors;
 
 import com.kauailabs.navx.frc.AHRS;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
@@ -17,115 +16,118 @@ import edu.wpi.first.wpilibj.SerialPort;
  * of the public getInstance() methods. They do
  * not return a value until the navX is connected
  * and calibrated.
+ * 
  * @author Shaked - ask me if you have questions🌠
  */
 public class HaNavX extends AHRS {
-    private HaNavX() {}
+	private HaNavX() {}
 
-    /**
-     * Starts communtication between navX and RoboRIO,
-     * enables logging to the RioLog & Driver Station,
-     * waits until the navX is connected and calibrated,
-     * then returns an instance.
-     * @param port serial port (usually USB)
-     * @return an instance of HaNavx, which extends AHRS.
-     */
-    public static HaNavX getInstance(SerialPort.Port port) {
-        AHRS navx = new AHRS(
-                port, SerialDataType.kProcessedData, (byte)60);
-        navx.enableLogging(true);
-        while(!navx.isConnected()) {}
-        while(navx.isCalibrating()) {}
-        DriverStation.reportError("navX done calibrating", false);
-        return (HaNavX)navx;
-    }
+	/**
+	 * Starts communtication between navX and RoboRIO,
+	 * enables logging to the RioLog & Driver Station,
+	 * waits until the navX is connected and calibrated,
+	 * then returns an instance.
+	 *
+	 * @param port serial port (usually USB)
+	 * @return an instance of HaNavx, which extends AHRS.
+	 */
+	public static HaNavX getInstance(SerialPort.Port port) {
+		AHRS navx = new AHRS(
+				port, SerialDataType.kProcessedData, (byte) 60);
+		navx.enableLogging(true);
+		while (!navx.isConnected()) {}
+		while (navx.isCalibrating()) {}
+		DriverStation.reportError("navX done calibrating", false);
+		return (HaNavX) navx;
+	}
 
-    /**
-     * Starts communtication between navX and RoboRIO,
-     * enables logging to the RioLog & Driver Station,
-     * waits until the navX is connected and calibrated,
-     * then returns an instance.
-     * @param port I2C port. Using the onboard I2C port
-     * is not reccomended, for more information click here:
-     * https://docs.wpilib.org/en/stable/docs/yearly-overview/known-issues.html#onboard-i2c-causing-system-lockups
-     * @return an instance of HaNavx, which extends AHRS.
-     */
-    public static HaNavX getInstance(I2C.Port port) {
-        AHRS navx = new AHRS(port);
-        navx.enableLogging(true);
-        while(!navx.isConnected()) {}
-        while(navx.isCalibrating()) {}
-        DriverStation.reportError("navX done calibrating", false);
-        return (HaNavX)navx;
-    }
+	/**
+	 * Starts communtication between navX and RoboRIO,
+	 * enables logging to the RioLog & Driver Station,
+	 * waits until the navX is connected and calibrated,
+	 * then returns an instance.
+	 * 
+	 * @param port I2C port. Using the onboard I2C port is not reccomended, for more information click here:
+	 *            https://docs.wpilib.org/en/stable/docs/yearly-overview/known-issues.html#onboard-i2c-causing-system-lockups
+	 * @return an instance of HaNavx, which extends AHRS.
+	 */
+	public static HaNavX getInstance(I2C.Port port) {
+		AHRS navx = new AHRS(port);
+		navx.enableLogging(true);
+		while (!navx.isConnected()) {}
+		while (navx.isCalibrating()) {}
+		DriverStation.reportError("navX done calibrating", false);
+		return (HaNavX) navx;
+	}
 
-    /**
-     * Starts communtication between navX and RoboRIO,
-     * enables logging to the RioLog & Driver Station,
-     * waits until the navX is connected and calibrated,
-     * then returns an instance.
-     * @param port SPI port
-     * @return an instance of HaNavx, which extends AHRS.
-     */
-    public static HaNavX getInstance(SPI.Port port) {
-        AHRS navx = new AHRS(port);
-        navx.enableLogging(true);
-        while(!navx.isConnected()) {}
-        while(navx.isCalibrating()) {}
-        DriverStation.reportError("navX done calibrating", false);
-        return (HaNavX)navx;
-    }
+	/**
+	 * Starts communtication between navX and RoboRIO,
+	 * enables logging to the RioLog & Driver Station,
+	 * waits until the navX is connected and calibrated,
+	 * then returns an instance.
+	 * 
+	 * @param port SPI port
+	 * @return an instance of HaNavx, which extends AHRS.
+	 */
+	public static HaNavX getInstance(SPI.Port port) {
+		AHRS navx = new AHRS(port);
+		navx.enableLogging(true);
+		while (!navx.isConnected()) {}
+		while (navx.isCalibrating()) {}
+		DriverStation.reportError("navX done calibrating", false);
+		return (HaNavX) navx;
+	}
 
-    /**
-     * @return The angle of the navX on the Z axis (perpendicular
-     * to earth, "yaw") in degrees. Rotating clockwise makes the
-     * the angle increase, and rotating counter-clockwise makes
-     * the angle decrease, according to WPILib's coordinate system
-     * conventions.
-     */
-    public double getYawAngleDegrees() {
-        return 360.0 - this.getYaw();
-    }
+	/**
+	 * @return The angle of the navX on the Z axis (perpendicular
+	 *         to earth, "yaw") in degrees. Rotating clockwise makes the
+	 *         the angle increase, and rotating counter-clockwise makes
+	 *         the angle decrease, according to WPILib's coordinate system
+	 *         conventions.
+	 */
+	public double getYawAngleDeg() {
+		return 360.0 - this.getYaw();
+	}
 
-    /**
-     * @return The angle of the navX on the Z axis (perpendicular
-     * to earth, "yaw") in radians. Rotating clockwise makes the
-     * angle increase, and rotating counter-clockwise makes the
-     * angle decrease, according to WPILib's coordinate system
-     * conventions.
-     */
-    public double getYawAngleRadians() {
-        return Math.toRadians(360.0 - this.getYaw());
-    }
+	/**
+	 * @return The angle of the navX on the Z axis (perpendicular
+	 *         to earth, "yaw") in radians. Rotating clockwise makes the
+	 *         angle increase, and rotating counter-clockwise makes the
+	 *         angle decrease, according to WPILib's coordinate system
+	 *         conventions.
+	 */
+	public double getYawAngleRad() {
+		return Math.toRadians(360.0 - this.getYaw());
+	}
 
-    /**
-     * @return The angle of the navX on the Z axis (perpendicular
-     * to earth, "yaw") as a Rotation2d. Rotating clockwise makes
-     * the angle increase, and rotating counter-clockwise makes
-     * the angle decrease, according to WPILib's coordinate system
-     * conventions.
-     */
-    public Rotation2d getYawRotation2d() {
-        return Rotation2d.fromDegrees(360.0 - this.getYaw());
-    }
+	/**
+	 * @return The angle of the navX on the Z axis (perpendicular
+	 *         to earth, "yaw") as a Rotation2d. Rotating clockwise makes
+	 *         the angle increase, and rotating counter-clockwise makes
+	 *         the angle decrease, according to WPILib's coordinate system
+	 *         conventions.
+	 */
+	public Rotation2d getYawRotation2d() {
+		return Rotation2d.fromDegrees(360.0 - this.getYaw());
+	}
 
-    /**
-     * @return the rate of change in the angle on the Z
-     *  axis (perpendicular to earth, "yaw") in degrees.
-     * Rotating clockwise returns a positive value, and
-     * counter-clockwise returns a negative value.
-     */
-    public double angularVelocityDegPerSec() {
-        return -this.getRate();
-    }
+	/**
+	 * @return the rate of change in the angle on the Z
+	 *         axis (perpendicular to earth, "yaw") in degrees.
+	 *         Rotating clockwise returns a positive value, and
+	 *         counter-clockwise returns a negative value.
+	 */
+	public double angularVelocityDegPS() {
+		return -this.getRate();
+	}
 
-    /**
-     * @return the rate of change in the angle on the Z
-     * axis (perpendicular to earth, "yaw") in radians.
-     * Rotating clockwise returns a positive value, and
-     * counter-clockwise returns a negative value.
-     */
-    public double angularVelocityRadPerSec() {
-        return Math.toRadians(-this.getRate());
-    }
+	/**
+	 * @return the rate of change in the angle on the Z
+	 *         axis (perpendicular to earth, "yaw") in radians.
+	 *         Rotating clockwise returns a positive value, and
+	 *         counter-clockwise returns a negative value.
+	 */
+	public double angularVelocityRadPS() {
+		return Math.toRadians(-this.getRate());
+	}
 }
--- HamosadLib/src/main/java/com/hamosad1657/lib/swerve/HaSwerveConstants.java
+++ HamosadLib/src/main/java/com/hamosad1657/lib/swerve/HaSwerveConstants.java
@@ -2,11 +2,11 @@ package com.hamosad1657.lib.swerve;
 
 public class HaSwerveConstants {
 	public static final double kTalonFXIntegratedEncoderTicksPerRev = 2048;
-	public static final double kTalonFXIntegratedEncoderTicksPerDegree = 2048/360.0;
+	public static final double kTalonFXIntegratedEncoderTicksPerDeg = 2048 / 360.0;
 
 	// Module angles for cross-locking the wheels
-	public static final double kFrontLeftCrossAngleDegrees = 45;
-	public static final double kFrontRightCrossAngleDegrees = 135;
-	public static final double kBackLeftCrossAngleDegrees = 135;
-	public static final double kBackRightCrossAngleDegrees = 45;
+	public static final double kFrontLeftCrossAngleDeg = 45;
+	public static final double kFrontRightCrossAngleDeg = 135;
+	public static final double kBackLeftCrossAngleDeg = 135;
+	public static final double kBackRightCrossAngleDeg = 45;
 }
