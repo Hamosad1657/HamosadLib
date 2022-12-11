42e5fb1e38a9499fe7b240fe9f502c78f712eccc
Mr.G8 <34008592+JordanG8@users.noreply.github.com>
2022-12-11 10:57:51 +0200

    cleanup

--- HamosadLib/src/main/java/com/hamosad1657/lib/PhotonVision.java
+++ HamosadLib/src/main/java/com/hamosad1657/lib/PhotonVision.java
@@ -1,6 +1,6 @@
 /**
- * <h1>This is the PhotonVision hamosad implementation. </h1>
- * it is used to retrive data from the photonvision pipeline
+ * <h1>This is the PhotonVision hamosad implementation.</h1>
+ * It is used to retrive data from the PhotonVision pipeline.
  */
 
 package com.hamosad1657.lib;
@@ -10,40 +10,46 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.geometry.Transform3d;
 
+/// Retrieves the robot's position from PhotonVision's pipeline.
 public class PhotonVision {
-    /// this is the PhotonCamera instance, name it after your camera name in photonvision
+    /// This is the PhotonCamera instance, name it after your camera name in photonvision.
     PhotonCamera camera = new PhotonCamera("MicrosoftCamera");
-    ///gets the robot's x (x is the further or closer the robot is reletive to the tag)
-    public double robotCurrentX() {
+
+    // Get the robot's x, y, and z.
+    public Transform3d getRobotTransform3D() {
         var result = camera.getLatestResult();
         boolean hasTargets = result.hasTargets();
         if (hasTargets) {
             PhotonTrackedTarget target = result.getBestTarget();
-            Transform3d bestCameraToTarget = target.getBestCameraToTarget();
-            return bestCameraToTarget.getX();
+           return target.getBestCameraToTarget();
         }
-        return 0.0;
+        return null;
     }
-    ///gets the robot's y (y is the further up or down the robot is reletive to the tag)
-    public double robotCurrentY() {
-        var result = camera.getLatestResult();
-        boolean hasTargets = result.hasTargets();
-        if (hasTargets) {
-            PhotonTrackedTarget target = result.getBestTarget();
-            Transform3d bestCameraToTarget = target.getBestCameraToTarget();
-            return bestCameraToTarget.getY();
+
+    /// Gets the robot's x (x is the further or closer the robot is reletive to the tag).
+    public Double robotCurrentX() {
+        var transform = this.getRobotTransform3D();
+        if (transform != null) {
+            return transform.getX();
         }
-        return 0.0;
+        return null;
     }
-    ///gets the robot's z (z is the further left or right the robot is reletive to the tag)
-    public double robotCurrentZ() {
-        var result = camera.getLatestResult();
-        boolean hasTargets = result.hasTargets();
-        if (hasTargets) {
-            PhotonTrackedTarget target = result.getBestTarget();
-            Transform3d bestCameraToTarget = target.getBestCameraToTarget();
-            return bestCameraToTarget.getZ();
+
+    /// Gets the robot's y (y is the further up or down the robot is reletive to the tag).
+    public Double robotCurrentY() {
+        var transform = this.getRobotTransform3D();
+        if (transform != null) {
+            return transform.getY();
         }
-        return 0.0;
+        return null;
+    }
+
+    /// Gets the robot's z (z is the further left or right the robot is reletive to the tag).
+    public Double robotCurrentZ() {
+        var transform = this.getRobotTransform3D();
+        if (transform != null) {
+            return transform.getZ();
+        }
+        return null;
     }
 }
