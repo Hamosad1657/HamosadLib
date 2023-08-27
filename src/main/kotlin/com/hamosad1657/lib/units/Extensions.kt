7097839f116a1c1128bf87dd7b60bc74350e957b
Sufilevy <sufilevy@gmail.com>
2023-08-27 20:32:56 +0300

    Added inline to extensions

--- src/main/kotlin/com/hamosad1657/lib/units/Extensions.kt
+++ src/main/kotlin/com/hamosad1657/lib/units/Extensions.kt
@@ -4,41 +4,41 @@ import edu.wpi.first.math.geometry.Rotation2d
 
 /// --- Length ---
 
-val Double.meters get() = Length.fromMeters(this)
-val Int.meters get() = Length.fromMeters(this.toDouble())
+inline val Double.meters get() = Length.fromMeters(this)
+inline val Int.meters get() = Length.fromMeters(this.toDouble())
 
-val Double.centimeters get() = Length.fromCentimeters(this)
-val Int.centimeters get() = Length.fromCentimeters(this.toDouble())
+inline val Double.centimeters get() = Length.fromCentimeters(this)
+inline val Int.centimeters get() = Length.fromCentimeters(this.toDouble())
 
-val Double.millimeters get() = Length.fromMillimeters(this)
-val Int.millimeters get() = Length.fromMillimeters(this.toDouble())
+inline val Double.millimeters get() = Length.fromMillimeters(this)
+inline val Int.millimeters get() = Length.fromMillimeters(this.toDouble())
 
-val Double.feet get() = Length.fromFeet(this)
-val Int.feet get() = Length.fromFeet(this.toDouble())
+inline val Double.feet get() = Length.fromFeet(this)
+inline val Int.feet get() = Length.fromFeet(this.toDouble())
 
-val Double.inches get() = Length.fromInches(this)
-val Int.inches get() = Length.fromInches(this.toDouble())
+inline val Double.inches get() = Length.fromInches(this)
+inline val Int.inches get() = Length.fromInches(this.toDouble())
 
 
 /// --- Angular Velocity ---
 
-val Double.rpm get() = AngularVelocity.fromRpm(this)
-val Int.rpm get() = AngularVelocity.fromRpm(this.toDouble())
+inline val Double.rpm get() = AngularVelocity.fromRpm(this)
+inline val Int.rpm get() = AngularVelocity.fromRpm(this.toDouble())
 
-val Double.radPs get() = AngularVelocity.fromRadPs(this)
-val Int.radPs get() = AngularVelocity.fromRadPs(this.toDouble())
+inline val Double.radPs get() = AngularVelocity.fromRadPs(this)
+inline val Int.radPs get() = AngularVelocity.fromRadPs(this.toDouble())
 
-val Double.degPs get() = AngularVelocity.fromDegPs(this)
-val Int.degPs get() = AngularVelocity.fromDegPs(this.toDouble())
+inline val Double.degPs get() = AngularVelocity.fromDegPs(this)
+inline val Int.degPs get() = AngularVelocity.fromDegPs(this.toDouble())
 
 
 /// -- Rotation2d ---
 
-val Double.degrees: Rotation2d get() = Rotation2d.fromDegrees(this)
-val Int.degrees: Rotation2d get() = Rotation2d.fromDegrees(this.toDouble())
+inline val Double.degrees: Rotation2d get() = Rotation2d.fromDegrees(this)
+inline val Int.degrees: Rotation2d get() = Rotation2d.fromDegrees(this.toDouble())
 
-val Double.radians: Rotation2d get() = Rotation2d.fromRadians(this)
-val Int.radians: Rotation2d get() = Rotation2d.fromRadians(this.toDouble())
+inline val Double.radians: Rotation2d get() = Rotation2d.fromRadians(this)
+inline val Int.radians: Rotation2d get() = Rotation2d.fromRadians(this.toDouble())
 
-val Double.rotations: Rotation2d get() = Rotation2d.fromRotations(this)
-val Int.rotations: Rotation2d get() = Rotation2d.fromRotations(this.toDouble())
+inline val Double.rotations: Rotation2d get() = Rotation2d.fromRotations(this)
+inline val Int.rotations: Rotation2d get() = Rotation2d.fromRotations(this.toDouble())
