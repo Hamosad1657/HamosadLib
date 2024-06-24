151d2c599da643f96a87b43abf387f8fd20bbe7e
ShakedShiboleth <shakedshibo@gmail.com>
2024-06-24 11:51:31 +0300

    Fixed runtime errors in LEDStrip

--- src/main/kotlin/com/hamosad1657/lib/leds/LEDStrip.kt
+++ src/main/kotlin/com/hamosad1657/lib/leds/LEDStrip.kt
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.Timer
 
 /** ONLY CREATE ONE INSTANCE OF THIS CLASS. */
 class LEDStrip(val length: Int, pwmPort: Int) {
+	private val maxIndex = length - 1
+
 	// --- LEDs ---
 
 	private val ledBuffer = AddressableLEDBuffer(length)
@@ -35,7 +37,7 @@ class LEDStrip(val length: Int, pwmPort: Int) {
 	// --- LEDs Control ---
 
 	fun applyColorForAll(color: RGBColor) {
-		for (i in 0..length) {
+		for (i in 0..maxIndex) {
 			bufferSetRGB(i, color)
 		}
 		ledStrip.setData(ledBuffer)
@@ -77,11 +79,11 @@ class LEDStrip(val length: Int, pwmPort: Int) {
 	}
 
 	/** If setting the entire strip, use [applyColorForAll]. */
-	fun applyColor(color: RGBColor, step: Int = 1, startIndex: Int = 0, endIndex: Int = length) {
+	fun applyColor(color: RGBColor, step: Int = 1, startIndex: Int = 0, endIndex: Int = maxIndex) {
 		val start = clampAndReportIndex(startIndex)
 		val end = clampAndReportIndex(endIndex)
 
-		for (i in start..<end step step) {
+		for (i in start..end step step) {
 			bufferSetRGB(i, color)
 		}
 		ledStrip.setData(ledBuffer)
@@ -92,7 +94,7 @@ class LEDStrip(val length: Int, pwmPort: Int) {
 	 * If toggling the entire strip together, use [toggleEntireStrip], it is more efficient.
 	 * @param color color to apply if a LED is off, instead of the last applied color.
 	 */
-	fun toggle(color: RGBColor, step: Int = 1, startIndex: Int = 0, endIndex: Int = length) {
+	fun toggle(color: RGBColor, step: Int = 1, startIndex: Int = 0, endIndex: Int = maxIndex) {
 		fun toggleSingleLED(index: Int) {
 			val currentColor = getColorForIndex(index)
 			val colorToApply = if (currentColor != LEDS_OFF) LEDS_OFF else color
@@ -112,7 +114,7 @@ class LEDStrip(val length: Int, pwmPort: Int) {
 	 * Applies the last applied color if a LED is off.
 	 * If toggling the entire strip together, use [toggleEntireStrip], it is more efficient.
 	 */
-	fun toggle(step: Int = 1, startIndex: Int = 0, endIndex: Int = length) {
+	fun toggle(step: Int = 1, startIndex: Int = 0, endIndex: Int = maxIndex) {
 		toggle(lastAppliedColor)
 	}
 
@@ -121,7 +123,7 @@ class LEDStrip(val length: Int, pwmPort: Int) {
 	 * If blinking the entire strip, use [blinkEntireStrip], it is more efficient.
 	 * @param color color to blink in, instead of the last applied color.
 	 */
-	fun blink(blinkTime: Seconds, color: RGBColor, step: Int = 1, startIndex: Int = 0, endIndex: Int = length) {
+	fun blink(blinkTime: Seconds, color: RGBColor, step: Int = 1, startIndex: Int = 0, endIndex: Int = maxIndex) {
 		blinkTimer.start()
 		if (blinkTimer.hasElapsed(blinkTime)) {
 			toggle(color, step, startIndex, endIndex)
@@ -133,19 +135,19 @@ class LEDStrip(val length: Int, pwmPort: Int) {
 	 * Should be called periodically.
 	 * If blinking the entire strip, use [blinkEntireStrip], it is more efficient.
 	 */
-	fun blink(blinkTime: Seconds, step: Int = 1, startIndex: Int = 0, endIndex: Int = length) {
+	fun blink(blinkTime: Seconds, step: Int = 1, startIndex: Int = 0, endIndex: Int = maxIndex) {
 		blink(blinkTime, lastAppliedColor, step, startIndex, endIndex)
 	}
-	
+
 	companion object {
 		val LEDS_OFF = RGBColor.BLACK
 	}
 
 	private fun clampAndReportIndex(index: Int): Int {
-		if (index in 0..length) return index
-		DriverStation.reportError("Index $index is out of range for length $length. Is being clamped between 0 and $length to avoid an error.",
+		if (index in 0..maxIndex) return index
+		DriverStation.reportError("Index $index is out of range for length $length. Is being clamped between 0 and $maxIndex to avoid an error.",
 			true)
-		return clamp(index, 0, length)
+		return clamp(index, 0, maxIndex)
 	}
 
 	private fun bufferSetRGB(index: Int, color: RGBColor) {
